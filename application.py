bf58f1f6b639bbd96b252705f4c1984a57ccd860
michaelthomasberry <michaelthomasberry@outlook.com>
2021-11-19 20:59:17 +0000

    we have added radio buttons on form, made the database search work. Very pleased :)

--- application.py
+++ application.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 db = SQL("sqlite:///cascades.db")
 
 
+
+#..... collect all data for output, layout, and hydraulic arrangement
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -13,6 +15,12 @@ def index():
 
 @app.route("/search", methods=["POST"])
 def search():
-    cascades = db.execute("SELECT * FROM cascades WHERE output= ?", request.form.get("pack") )
-    rows = db.execute("SELECT * FROM bom JOIN cascades ON bom.bom_id = cascades.id WHERE output = ?", request.form.get("pack") )
+    #..... grab the data from the form and save user inputs as variables
+    pack = request.form.get("pack")
+    hydraulic = request.form.get("hydraulic")
+    layout = request.form.get("layout")
+    #..... show the casacade pack from cascades table in the database that matches the users perameters 
+    cascades = db.execute("SELECT * FROM cascades WHERE output= ? AND hydraulic = ? AND layout =?", pack, hydraulic, layout,)
+    #..... show the BOM of that pack
+    rows = db.execute("SELECT * FROM bom JOIN cascades ON bom.bom_id = cascades.id WHERE output = ? AND hydraulic = ? AND layout =?", pack, hydraulic, layout,)
     return render_template("search.html",  cascades = cascades, rows = rows)
